fbfac93d030dfea3ba621c649529deeb99b3ca3c
toth-kevin <tothkevint15@gmail.com>
2025-09-27 23:40:11 +0200

    Tízszög rajzolás Második proba

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import turtle
 while True:
-    meret_input = input("Add meg az oldal hosszát (csak pozitív egész szám): ")
+    meret_input = input("Kérlek add meg az oldal hosszát (csak pozitív egész szám): ")
     if meret_input.isdigit() and int(meret_input) > 0:
         meret = int(meret_input)
         break
@@ -10,18 +10,17 @@ while True:
         print("Hibás adat, kérlek próbáld újra!")
 
 ablak = turtle.Screen()
-ablak.title("Szabályos tízszög")
+ablak.title("Szabályos tízszög rajzolás")
 
 t = turtle.Turtle()
 t.color("blue")
 t.pensize(3)
 t.speed(5)
 t.penup()
-t.goto(-meret/2, -meret/2)  # kb. középre igazít
+t.goto(-meret/2, -meret/2)
 t.pendown()
 
 
-# Szabályos tízszög
 for _ in range(10):
     t.forward(meret)
     t.left(36)   # 360 / 10 = 36 fok
